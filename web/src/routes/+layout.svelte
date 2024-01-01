bf58c2075bc947033a764d065192f37d4be988a4
Eun Shang Lee <leetdavidu@gmail.com>
2024-01-01 14:04:40 +0800

    Refactor layout component and add command dialog

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,8 +1,30 @@
 <script lang="ts">
-  import { ModeWatcher } from 'mode-watcher';
-  import Header from '$lib/components/Header.svelte';
-  import Container from '$lib/components/Container.svelte';
   import '../app.pcss';
+  import { ModeWatcher } from 'mode-watcher';
+  import Header from '$components/Header.svelte';
+  import * as Command from '$components/ui/command';
+  import Container from '$components/Container.svelte';
+  import { onMount } from 'svelte';
+  let open = false;
+  let loading = false;
+
+  onMount(() => {
+    function handleKeydown(e: KeyboardEvent) {
+      if (e.key === 'k' && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        open = !open;
+      }
+    }
+
+    document.addEventListener('keydown', handleKeydown);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeydown);
+    };
+  });
+
+  import { Menu } from 'lucide-svelte';
+  import { goto } from '$app/navigation';
 </script>
 
 <ModeWatcher />
@@ -10,3 +32,26 @@
 <Container>
   <slot />
 </Container>
+
+<Command.Dialog bind:open>
+  <Command.Input placeholder="Type a command or search..." />
+  <Command.List>
+    {#if loading}
+      <Command.Loading>Loading...</Command.Loading>
+    {/if}
+
+    <Command.Empty>No results found.</Command.Empty>
+
+    <Command.Group heading="Counter-Strike 2 Stuff">
+      <Command.Item
+        onSelect={(value) => {
+          goto(`/cs2/${value}`);
+          open = false;
+        }}
+      >
+        <Menu class="mr-2 h-4 w-4" />
+        <span>Callouts</span>
+      </Command.Item>
+    </Command.Group>
+  </Command.List>
+</Command.Dialog>
